cb3d45c4260f090038f5fbe2865757f7fee480c0
arjunkahar02111-alt <arjunkahar02111@gmail.com>
2026-07-12 19:26:14 +0530

    Delete supabase/migrations/supabase/migrations directory

--- supabase/migrations/supabase/migrations/20260712133752_38947f33-a896-4f6a-844f-f33387f1a03c.sql
+++
@@ -1,4 +0,0 @@
-GRANT ALL ON public.admin_settings TO service_role;
-GRANT ALL ON public.blocked_ips TO service_role;
-GRANT ALL ON public.lookup_logs TO service_role;
-NOTIFY pgrst, 'reload schema';
